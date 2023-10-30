7b7e7f9a982b86637a557343d36046a031cfb0c3
Bingyan He (Harper) <143116415+harperdaboss@users.noreply.github.com>
2023-10-30 13:50:55 -0700

    Create chorodata.js

---
+++ chorodata.js
@@ -0,0 +1,9 @@
+const data = [
+  {"Country": "China", "Value": 9446.25},
+  {"Country": "Hong Kong", "Value": 46977.5},
+  {"Country": "Japan", "Value": 38726.0},
+  {"Country": "South Korea", "Value": 31682.0},
+  {"Country": "Singapore", "Value": 60748.75}
+];
+
+module.exports = data;
