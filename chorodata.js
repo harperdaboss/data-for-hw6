2097ee92a89ed30ae50967c27bd2c53944f191b4
Bingyan He (Harper) <143116415+harperdaboss@users.noreply.github.com>
2023-10-30 13:53:28 -0700

    Update chorodata.js

--- chorodata.js
+++ chorodata.js
@@ -1,9 +1,6 @@
-const data = [
-  {"Country": "China", "Value": 9446.25},
-  {"Country": "Hong Kong", "Value": 46977.5},
-  {"Country": "Japan", "Value": 38726.0},
-  {"Country": "South Korea", "Value": 31682.0},
-  {"Country": "Singapore", "Value": 60748.75}
-];
-
-module.exports = data;
+Country	Value
+China	9446.25
+Hong Kong	46977.5
+Japan	38726
+South Korea	31682
+Singapore	60748.75
